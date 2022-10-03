388f20e1cffe775f8c25afaaa89ddd4f52569c62
Michael Morriss <61882610+mcmorriss@users.noreply.github.com>
2022-10-03 17:39:49 -0500

    Create isHappy.py

---
+++ isHappy.py
@@ -0,0 +1,18 @@
+# Date: October 3rd, 2022
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        nums = {n}
+        while True:
+            total = 0
+            for i in str(n):
+                total += int(i) ** 2
+            if total == 1:
+                return True
+            if total in nums:
+                return False
+            nums.add(total)
+            n = total
+            
+            # Runtime: 67 ms, faster than 33.38% of Python3 online submissions for Happy Number.
+            # Memory Usage: 13.8 MB, less than 96.79% of Python3 online submissions for Happy Number.
