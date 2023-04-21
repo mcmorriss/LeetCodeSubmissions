90406f7af2c4ce74aee65386c8fce9ee0772d41c
Michael Morriss <61882610+mcmorriss@users.noreply.github.com>
2023-04-20 23:46:44 -0500

    Update pascalsTriangle.py

--- DynamicProgramming/pascalsTriangle.py
+++ DynamicProgramming/pascalsTriangle.py
@@ -1,12 +1,16 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
+ 
         if numRows == 1:
             return [[1]]
         elif numRows == 2:
             return [[1], [1, 1]]
-        triangle = [[0]] * numRows
-        triangle[0] = [1]
-        triangle[1] = [1, 1]
+        else:
+            # Memoization, tracking answers to subproblems.
+            triangle = [[0]] * numRows
+            triangle[0] = [1]
+            triangle[1] = [1, 1]
+
         for i in range(2, numRows):
             row = [1] * (i + 1)
             x = 1
@@ -14,4 +18,5 @@ class Solution:
                 row[x] = triangle[i - 1][x] + triangle[i - 1][x - 1]
                 x += 1
             triangle[i] = row
+            
         return triangle
